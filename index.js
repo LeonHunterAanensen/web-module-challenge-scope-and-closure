2315772c7d3ced3cd7736bd126651919efdc1e85
Leon Aanensen <lhaworking@gmail.com>
2022-11-26 13:03:37 -0500

    doubt

--- index.js
+++ index.js
@@ -116,11 +116,11 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-
+function getInningScore(In) {
+  J = {"Home":In,
+       "Away":In}
 }
-
+getInningScore(inning())
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
